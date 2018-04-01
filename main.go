6e9ccb671611c604dd94afb477fdb10a390e1d07
Sanderhelleso <sanderhelleso@hotmail.com>
2018-04-01 02:09:47 +0200

    added func to update a specific book, found by ID

--- main.go
+++ main.go
@@ -65,7 +65,22 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // update a book
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	// find book and update
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // delete book
